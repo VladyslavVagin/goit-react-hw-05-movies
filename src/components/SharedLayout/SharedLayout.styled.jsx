38128a49a01d304d306c31847e89cc9b487bce9f
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-19 08:49:29 +0100

    update

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -4,7 +4,7 @@ export const Container = styled.div`
     padding: 0 20px;
 `
 export const Header = styled.header`
-    padding: 24px 0;
+    padding: 20px 0;
     border-bottom: 2px solid black;
     box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
     background-color: #f3f2f2;
