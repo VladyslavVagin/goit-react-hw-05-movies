135cb0eaaa2f56aceb6bf0d64259f04140fc1070
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-21 12:45:52 +0100

    update

--- src/components/MovieDetails/MovieDetails.styled.jsx
+++ src/components/MovieDetails/MovieDetails.styled.jsx
@@ -34,9 +34,34 @@ export const BackBox = styled.div`
 
 export const AdditionalInfo = styled.h4`
   font-size: 1.5rem;
+  text-shadow: 
+    1px 0px 1px #ccc, 0px 1px 1px #eee, 
+    2px 1px 1px #ccc, 1px 2px 1px #eee,
+    3px 2px 1px #ccc, 2px 3px 1px #eee,
+    4px 3px 1px #ccc, 3px 4px 1px #eee,
+    5px 4px 1px #ccc, 4px 5px 1px #eee,
+    6px 5px 1px #ccc, 5px 6px 1px #eee,
+    7px 6px 1px #ccc;
 `;
 export const NameMovie = styled.h4`
   font-size: 2rem;
+  text-shadow:
+    0 1px #808d93,
+    -1px 0 #cdd2d5,
+    -1px 2px #808d93,
+    -2px 1px #cdd2d5,
+    -2px 3px #808d93,
+    -3px 2px #cdd2d5,
+    -3px 4px #808d93,
+    -4px 3px #cdd2d5,
+    -4px 5px #808d93,
+    -5px 4px #cdd2d5,
+    -5px 6px #808d93,
+    -6px 5px #cdd2d5,
+    -6px 7px #808d93,
+    -7px 6px #cdd2d5,
+    -7px 8px #808d93,
+    -8px 7px #cdd2d5;
 `;
 export const UserScore = styled.span`
   border: 1px solid black;
