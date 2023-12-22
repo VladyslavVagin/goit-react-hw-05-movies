66cfdf8ebd868ed2504bad8d3119243524f035bb
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-22 09:56:07 +0100

    update

--- src/components/Reviews/Reviews.styled.jsx
+++ src/components/Reviews/Reviews.styled.jsx
@@ -13,6 +13,7 @@ export const AuthorName = styled.h4`
     background-color: #696969;
     color: #ffffff;
     letter-spacing: 0.08em;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
 `
 export const NickName = styled.span`
     color: black;
--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -5,7 +5,7 @@ export const Container = styled.div`
 `
 export const Header = styled.header`
     padding: 20px 0;
-    border-bottom: 2px solid black;
+    border-bottom: 2px solid #797777;
     background-color: #cacaca;
     font-weight: 700;
 `
